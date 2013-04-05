896313340cffe23052b4d8bb5bac3285c84a663a
xiaoji121 <dong.fly.ming@gmail.com>
2013-04-05 16:33:44 +0800

    ff

---
+++ site/js/views/book.js
@@ -0,0 +1,25 @@
+var app = app || {};
+
+app.BookView = Backbone.View.extend({
+	tagName: 'div',
+	className: 'bookContainer',
+	template: $('#bookTemplate').html(),
+
+	events: {
+		'click .delete': 'deleteBook'
+	},
+
+	render: function() {
+		var tmpl = _.template( this.template );
+
+		this.$el.html( tmpl( this.model.toJSON() ) );
+
+		return this;
+	},
+
+	deleteBook: function() {
+		this.model.destroy();
+
+		this.remove();
+	}
+})
